52be438fc081eb56a5e68c745de495d6388c1c17
Elif <elfgmlk@gmail.com>
2023-02-26 14:20:22 +0100

    Tekrar eklendi

--- src/Elif/ben.java
+++ src/Elif/ben.java
@@ -3,5 +3,8 @@ package Elif;
 public class ben {
     public static void main(String[] args) {
         System.out.println("simdi mavi olacak");
+
+        int a= 5;
+        System.out.println("a = " + a);
     }
 }
